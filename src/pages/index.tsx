e32e9ce5b639463e5e47cefae5ce422c5ea7198a
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2021-06-06 15:18:42 -0300

    getting subscription price from stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
+import { GetServerSideProps, GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SignInButton } from '../components/SignInButton'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: string
+  }
+}
+
+export default function Home(props: HomeProps) {
+  const { product } = props
+
   return (
     <>
       <Head>
@@ -21,14 +32,28 @@ export default function Home() {
           <p>
             Get access to all the publications
             <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          
-          <SubscribeButton />
+
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src='/images/avatar.svg' alt='Girl coding' />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Iz1riCOf1N7u7kcTcdmN2ep')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return { props: { product }, revalidate: 60 * 60 * 24 }
 }

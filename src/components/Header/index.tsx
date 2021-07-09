10c84d96c28b833c24cd55d3fd8848f7aec4627a
Caio Medeiros <caio.quirino.medeiros@gmail.com>
2021-07-09 15:24:02 -0300

    posts page and integrated with Prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,24 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
+  const { pathname, route } = useRouter()
+  console.log({ pathname, route })
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src='/images/logo.svg' alt='ig.news' />
 
         <nav>
-          <a className={styles.active} href='/'>
-            Home
-          </a>
-          <a href='/posts'>Posts</a>
+          <Link href={'/'}>
+            <a className={pathname === '/' ? styles.active : undefined}>Home</a>
+          </Link>
+          <Link href={'/posts'}>
+            <a className={pathname === '/posts' ? styles.active : undefined}>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
